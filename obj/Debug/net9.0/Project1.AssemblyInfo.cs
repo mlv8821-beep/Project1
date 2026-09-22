25b531bb8ff5a2cee774ce44015ad5c78f257b26
mlv8821-beep <mlv8821@nyu.edu>
2026-09-22 15:01:14 -0700

    ported

--- obj/Debug/net9.0/Project1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdf7043c111d6ba81b6c29f8dd2ed76fe30ef94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
